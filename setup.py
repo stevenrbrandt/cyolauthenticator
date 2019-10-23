edce6a65e7bc5e37a22b79c27e31c061a137e070
Steven R. Brandt <sbrandt@cct.lsu.edu>
2019-10-23 08:28:49 -0500

    Add dependency

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 setup(
   name='cyolauthenticator',
   version='1.0',
-  description='A Jupyterhub Authenticator that allows users to pick a name and password if they know a code'
-  long_description='A Jupyterhub Authenticator that allows users to pick a name and password if they know a code'
+  description='A Jupyterhub Authenticator that allows users to pick a name and password if they know a code',
+  long_description='A Jupyterhub Authenticator that allows users to pick a name and password if they know a code',
   url='http://cct.lsu.edu/~sbrandt/',
   author='Steven R. Brandt',
   author_email='steven@stevenrbrandt.com',
   license='LGPL',
-  packages=['cyolauthenticator','PAM']
+  packages=['cyolauthenticator'],
+  install_requires=["PAM"]
 )
