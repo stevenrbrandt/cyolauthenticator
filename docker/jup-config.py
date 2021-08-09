72e20759b2f9c5007411b1c28e2723aa35b20442
Steven R. Brandt <sbrandt@cct.lsu.edu>
2021-08-09 15:07:26 -0500

    Generate a key at startup

--- docker/jup-config.py
+++ docker/jup-config.py
@@ -1,4 +1,16 @@
-#import os
+import os
+from traceback import print_exc
+from subprocess import Popen, PIPE
+
+def call(cmd, inp=None):
+    print("Calling:",cmd)
+    if inp is None:
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    else:
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE, stdin=PIPE, universal_newlines=True)
+    out, err = p.communicate(inp)
+    print(out,end='',flush=True)
+    print(err,end='',flush=True)
 
 c.JupyterHub.authenticator_class = 'cyolauthenticator.CYOLAuthenticator'
 c.JupyterHub.template_paths = ['/jinja/templates']
@@ -6,10 +18,31 @@ c.JupyterHub.template_paths = ['/jinja/templates']
 # openssl genrsa -out rootCA.key 2048
 # openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.pem
 
-import os
-if os.path.exists('/etc/pki/tls/certs/tutorial.cer'):
-  c.JupyterHub.ssl_cert = '/etc/pki/tls/certs/tutorial.cer'
-  c.JupyterHub.ssl_key =  '/etc/pki/tls/private/tutorial.key'
+certname = os.environ.get('CERT_NAME','tutorial')
+cer = f'/etc/pki/tls/certs/{certname}.cer'
+key = f'/etc/pki/tls/private/{certname}.key'
 
-# Uncomment if needed
-#c.JupyterHub.base_url = '/somename/'
+try:
+
+  if not os.path.exists(key):
+    os.makedirs(os.path.dirname(key), exist_ok=True)
+    call(["openssl","genrsa","-out",key,"2048"])
+
+  if not os.path.exists(cer):
+    os.makedirs(os.path.dirname(cer), exist_ok=True)
+    answers = open("info.txt").read()
+    call(["openssl","req","-x509","-new","-nodes","-key",key,"-sha256","-days","1024","-out",cer],answers)
+
+  if os.path.exists(cer) and os.path.exists(key):
+    print("Starting with SSL")
+    c.JupyterHub.ssl_cert = cer
+    c.JupyterHub.ssl_key =  key
+  else:
+    print("Starting without SSL")
+
+
+  # Uncomment if needed
+  #c.JupyterHub.base_url = '/somename/'
+except:
+  print_exc()
+print(" >>> CONFIGURATION COMPLETE <<<<")
--- docker/startup.sh
+++ docker/startup.sh
@@ -4,11 +4,5 @@ python /usr/local/bin/frame.py
 
 #cp --update /home/passwd /home/group /home/shadow /etc/
 
-# If we have a certificate directory...
-if [ -d /etc/pki/tls/certs/tutorial.cer ]
-then
-  PORT=443
-else
-  PORT=80
-fi
+PORT=443
 jupyterhub --ip 0.0.0.0 --port $PORT -f jup-config.py
