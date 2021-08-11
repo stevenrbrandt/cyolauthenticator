d59d973da8fc54b97643bdc7c21f1406de188f14
Steven R. Brandt <sbrandt@cct.lsu.edu>
2021-08-11 14:16:19 -0500

    Increase version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='cyolauthenticator',
-  version='1.0',
+  version='1.1',
   description='A Jupyterhub Authenticator that allows users to pick a name and password if they know a code',
   long_description='A Jupyterhub Authenticator that allows users to pick a name and password if they know a code',
   url='http://cct.lsu.edu/~sbrandt/',
