c39587fc807bbcf708f74483547df20f44ee9e08
Steven R. Brandt <sbrandt@cct.lsu.edu>
2023-06-17 17:02:29 -0500

    Simplify check for an existing user

--- cyolauthenticator/cyolauthenticator.py
+++ cyolauthenticator/cyolauthenticator.py
@@ -84,48 +84,44 @@ def mkuser(user, passw, passw2, code_check):
       e.my_message = "Illegal character in password. Only letters, numbers and the underscore are allowed."
       raise e
 
-    home = "/home/%s" % user
-    cmd = ["useradd",user,"-s","/bin/bash"]
+    #home = "/home/%s" % user
+    #cmd = ["useradd",user,"-s","/bin/bash"]
     check_pass2 = False
-    if os.path.exists(home):
-      uid = stat(home).st_uid
-      try:
-        pwd.getpwuid(uid)
-        return authuser(user, passw)
-        # The user already exists, nothing to do
-        #return uid
-      except KeyError:
-        check_pass2 = True
-      cmd += ["-u",str(uid)]
-    else:
-
-      if not os.path.exists("/usr/enable_mkuser"):
-        e = HTTPError(403)
-        e.my_message = "MkUser disabled"
-        raise e
-      if not code_check:
-         e = HTTPError(403)
-         e.my_message = "Code check failed"
-         raise e
+    try:
+      pwd.getpwnam(user)
+      return authuser(user, passw)
+      # The user already exists, nothing to do
+      #return uid
+    except KeyError:
       check_pass2 = True
-      if passw != passw2:
-        e = HTTPError(403)
-        e.my_message = "Password and Password2 do not match."
-        raise e
-      cmd += ["-m"]
-      uids = set()
-      for path in os.listdir("/home"):
-        u = stat("/home/%s" % path).st_uid
-        uids.add(u)
-      for u in range(1000,100000):
-        if u in uids:
-          continue
-        try:
-          pwd.getpwuid(u)
-        except KeyError:
-          uid = u
-          cmd += ["-u",str(uid)]
-          break
+
+    if not os.path.exists("/usr/enable_mkuser"):
+      e = HTTPError(403)
+      e.my_message = "MkUser disabled"
+      raise e
+    if not code_check:
+       e = HTTPError(403)
+       e.my_message = "Code check failed"
+       raise e
+    check_pass2 = True
+    if passw != passw2:
+      e = HTTPError(403)
+      e.my_message = "Password and Password2 do not match."
+      raise e
+    #cmd += ["-m"]
+    uids = set()
+    for path in os.listdir("/home"):
+      u = stat("/home/%s" % path).st_uid
+      uids.add(u)
+    for u in range(1000,100000):
+      if u in uids:
+        continue
+      try:
+        pwd.getpwuid(u)
+      except KeyError:
+        uid = u
+        #cmd += ["-u",str(uid)]
+        break
 
     if check_pass2:
       if passw != passw2:
