bc7e1f402d7eb972420e289a9394be39c318ce4d
Steven R. Brandt <sbrandt@cct.lsu.edu>
2023-06-17 16:34:51 -0500

    Fix setting of PYTHONPATH

--- docker/startup.sh
+++ docker/startup.sh
@@ -13,7 +13,7 @@ chmod 600 /etc/ldap-pw.txt
 unset LDAP_READONLY_USER_PASSWORD
 unset LDAP_ADMIN_PASSWORD
 
-export PYTHONPATH=/usr/local/lib/python$(python3 -c 'import sys; print("%d.%d" % (sys.version_info.major, sys.version_info.minor))')/dist-packages/cyolauthenticator
+export PYTHONPATH=/usr/local/lib/python$(python3 -c 'import sys; print("%d.%d" % (sys.version_info.major, sys.version_info.minor))')/dist-packages
 
 PORT=443
 echo jupyterhub --ip 0.0.0.0 --port $PORT -f jup-config.py
